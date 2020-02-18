f386a5746aad65118fa4d375d88b248d0b63b0c0
Kevin Russell <kevinfrussell@gmail.com>
2020-02-17 20:14:07 -0500

    Add falling edge detect

--- src/main/cpp/utility/StickySwitch.cpp
+++ src/main/cpp/utility/StickySwitch.cpp
@@ -16,12 +16,26 @@ bool cwtech::StickySwitch::GetPressed()
     return false;
 }
 
+bool cwtech::StickySwitch::GetReleased() 
+{
+    if(m_released)
+    {
+        m_released = false;
+        return true;
+    }
+    return false;
+}
+
 void cwtech::StickySwitch::ProcessForPressed()
 {
     auto currentState = Get();
-    if(m_pressed == false && currentState == true && m_lastRead == false)
+    if(m_pressed == false && currentState == false && m_lastRead == true)
     {
         m_pressed = true;
+    }
+    if(m_released == false && currentState == true && m_lastRead == false)
+    {
+        m_released = true;
     }
     m_lastRead = currentState;
 }
--- src/main/include/utility/StickySwitch.h
+++ src/main/include/utility/StickySwitch.h
@@ -9,10 +9,13 @@ class StickySwitch : public frc::DigitalInput
 {
 private:
     bool m_pressed;
+    bool m_released;
     bool m_lastRead;
 public:
     StickySwitch(int port);
+
     bool GetPressed();
+    bool GetReleased();
     void ProcessForPressed();
 };
 
