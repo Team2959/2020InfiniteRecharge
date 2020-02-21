273439b65276a99d06709acac38a581934c1fad2
samueldaley <stallionsamueld@gmail.com>
2020-02-21 18:19:05 -0500

    Fixed indentation in UsbSerialPort.h

--- src/main/include/utility/UsbSerialPort.h
+++ src/main/include/utility/UsbSerialPort.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h> 
-#include <errno.h> 
+#include <fcntl.h>
+#include <errno.h>
 #include <termios.h>
 #include <bitset>
 #include <streambuf>
@@ -16,50 +16,52 @@ namespace cwtech
 {
 
 class UsbSerialPort
-{	
+{
 private:
 	bool m_status;
 	int m_usb_fd;
+
 public:
 	UsbSerialPort(std::string target, speed_t baudrate)
 		: m_status(true)
 	{
-		int USB = open(target.c_str(), O_RDWR| O_NOCTTY );
+		int USB = open(target.c_str(), O_RDWR | O_NOCTTY);
 
 		struct termios tty;
 		struct termios tty_old;
-		memset (&tty, 0, sizeof tty);
-		
+		memset(&tty, 0, sizeof tty);
+
 		/* Error Handling */
-		if ( tcgetattr ( USB, &tty ) != 0 ) 
+		if (tcgetattr(USB, &tty) != 0)
 		{
 			m_status = false;
 		}
-		
+
 		/* Save old tty parameters */
 		tty_old = tty;
-		
+
 		/* Set Baud Rate */
-		cfsetospeed (&tty, baudrate);
-		cfsetispeed (&tty, baudrate);
-		
+		cfsetospeed(&tty, baudrate);
+		cfsetispeed(&tty, baudrate);
+
 		/* Set Parameters */
-		tty.c_cflag     &=  ~PARENB;
-		tty.c_cflag     &=  ~CSTOPB;
-		tty.c_cflag     &=  ~CSIZE;
-		tty.c_cflag     |=  CS8;
-		
-		tty.c_cflag     &=  ~CRTSCTS;           
-		tty.c_cc[VMIN]   =  1;                  
-		tty.c_cc[VTIME]  =  5;                  
-		tty.c_cflag     |=  CREAD | CLOCAL;     
-		
+		tty.c_cflag &= ~PARENB;
+		tty.c_cflag &= ~CSTOPB;
+		tty.c_cflag &= ~CSIZE;
+		tty.c_cflag |= CS8;
+
+		tty.c_cflag &= ~CRTSCTS;
+		tty.c_cc[VMIN] = 1;
+		tty.c_cc[VTIME] = 5;
+		tty.c_cflag |= CREAD | CLOCAL;
+
 		/* Make raw */
 		cfmakeraw(&tty);
-		
+
 		/* Flush Port, then applies attributes */
-		tcflush( USB, TCIFLUSH );
-		if ( tcsetattr ( USB, TCSANOW, &tty ) != 0) {
+		tcflush(USB, TCIFLUSH);
+		if (tcsetattr(USB, TCSANOW, &tty) != 0)
+		{
 			m_status = false;
 		}
 
@@ -70,43 +72,39 @@ public:
 	std::string Read()
 	{
 		char buf = '\0';
-		std::string result;	
+		std::string result;
 		int n = 0;
 
-		do 
+		do
 		{
 			n = read(m_usb_fd, &buf, 1);
 			result.push_back(buf);
-		} while(buf != '\r' && n > 0);
+		} while (buf != '\r' && n > 0);
 
-		if(n < 0)
+		if (n < 0)
 		{
 			m_status = false;
 		}
 
-		return result;	
+		return result;
 	}
 
 	void Write(std::string msg)
 	{
 		int n = 0;
-		for(auto& c : msg)
+		for (auto &c : msg)
 		{
 			n = write(m_usb_fd, &c, 1);
-			if(n <= 0)
+			if (n <= 0)
 			{
 				break;
 			}
 		}
-		if(n < 0)
+		if (n < 0)
 		{
 			m_status = false;
 		}
 	}
-
-
 };
 
-}
-
-
+} // namespace cwtech
